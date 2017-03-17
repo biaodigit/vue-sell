b271c4a3d7c08021c79d1f02d79f3e3b1ee7ff51
wendigit <15622285015@163.com>
2017-03-17 09:35:37 +0800

    完成初始准备工作

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      i am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue';
+
+  export default {
+    components:{
+      'v-header':header
+    }
+  };
+</script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+   #app
+    .tab
+      display:flex
+      width:100%
+      height:40px
+      line-height:40px
+      .tab-item
+        flex:1
+        text-align:center
+</style>
